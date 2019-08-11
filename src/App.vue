afafa9a08397a3a092b9d7dd0973e22977d995d6
Tarun <tarunbhatt9784@gmail.com>
2019-08-11 11:59:04 +1000

    'as'

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+      <PolicyComponent/>
+</div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import PolicyComponent from './components/PolicyComponent.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    PolicyComponent
   }
 }
 </script>
---
+++ src/components/PolicyComponent.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  
+  <p>{{myJson}}</p>
+  </div>
+</template>
+
+<script>
+import json from './../json/policy-owners.json'
+export default {
+    name: "PolicyComponent",
+    data() {
+      return {
+        msg: "My Profile Component",
+        myJson: json
+      };
+    }
+  };
+</script>
+
